d5733857de42a2e441c0fea36fae0d42fb56eeb4
Kaan Karagür <kaankarg08@gmail.com>
2025-04-06 20:33:53 +0300

    shader support

--- ez.h
+++ ez.h
@@ -1,18 +1,19 @@
 #include <stdint.h>
+#include <stdlib.h>
 
 typedef struct RGBA
 {
-    uint8_t r,g,b,a;
+    uint8_t r, g, b, a;
 } RGBA;
 
 typedef struct floatRGBA
 {
-    float r,g,b,a;
+    float r, g, b, a;
 } floatRGBA;
 
-typedef struct Size 
+typedef struct Size
 {
-    uint16_t x,y;
+    uint16_t x, y;
 } Size;
 
 /**
@@ -21,23 +22,27 @@ typedef struct Size
  * @param y The y dimension of the size (uint16_t)
  * @warning uint16_t supports the numbers between 0 and 65535
  */
-Size generateSize(uint16_t x,uint16_t y){
-    return (Size){x,y};
+Size generateSize(uint16_t x, uint16_t y)
+{
+    return (Size){x, y};
 }
 
 RGBA generateRGBA(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
 double clamp(double d, double min, double max);
 float mapRGBA(uint8_t x);
-void windowColor(uint8_t r,uint8_t b,uint8_t g,uint8_t a);
+void setWindowColorRGBA(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+
+char *readShaderFromFile(const char *filePath);
 
 int setupGLAD();
-int setupWindow(Size size, char* name);
+int setupWindow(Size size, char *name);
 void setupViewport(Size size);
 
 /**
  * Runs glfwInit() and some WindowHints (You may need to change them by yourself)
  */
-void setupGLFW(){
+void setupGLFW()
+{
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -45,8 +50,15 @@ void setupGLFW(){
 }
 
 GLFWwindow *window;
-void setEzWindow(GLFWwindow *x){
+void setEzWindow(GLFWwindow *x)
+{
     window = x;
+}
+
+void updateWindow()
+{
+    glfwPollEvents();
+    glfwSwapBuffers(window);
 }
 
 /**
@@ -54,7 +66,8 @@ void setEzWindow(GLFWwindow *x){
  * @param size The Size type that has x and y for scaling
  * @param name Window title
  */
-int setupWindow(Size size, char* name){
+int setupWindow(Size size, char *name)
+{
     GLFWwindow *window = glfwCreateWindow(size.x, size.y, name, NULL, NULL);
     if (window == NULL)
     {
@@ -66,11 +79,11 @@ int setupWindow(Size size, char* name){
     setEzWindow(window);
 }
 
-
 /**
  * Setups glad and returns -1 if it fails
  */
-int setupGLAD(){
+int setupGLAD()
+{
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         printf("Failed to initialize GLAD\n");
@@ -88,38 +101,39 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height)
  * @param size The Size type that includes the x and y for scaling
  * Also calls the framebuffer_size_callback for feedback (from ez.h)
  */
-void setupViewport(Size size){
+void setupViewport(Size size)
+{
     glViewport(0, 0, size.x, size.y);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 }
-
 
 /**
  * Generating RGBA typedef
  * @param all unsigned int, a number between 0 and 255
  */
-RGBA generateRGBA(uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
-    return (RGBA){r,g,b,a};
+RGBA generateRGBA(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+{
+    return (RGBA){r, g, b, a};
 }
 
 /**
  * Convert RGBA type to floatRGBA type
  * @param color The RGBA type that will get converted
  */
-floatRGBA generateFloatRGBA(RGBA color){
-    return (floatRGBA){mapRGBA(color.r),mapRGBA(color.g),mapRGBA(color.b),mapRGBA(color.a)};
+floatRGBA generateFloatRGBA(RGBA color)
+{
+    return (floatRGBA){mapRGBA(color.r), mapRGBA(color.g), mapRGBA(color.b), mapRGBA(color.a)};
 }
 
 /**
  * Set window background with simple RGBA
  * @param all RGBA parameters which require a number between 0 and 255
  */
-void windowColorRGBA(uint8_t r,uint8_t g,uint8_t b,uint8_t a)
+void setWindowColorRGBA(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 {
-    floatRGBA color1 = generateFloatRGBA(generateRGBA(r,g,b,a));
-    glClearColor(color1.r,color1.g,color1.b,color1.a);
+    floatRGBA color1 = generateFloatRGBA(generateRGBA(r, g, b, a));
+    glClearColor(color1.r, color1.g, color1.b, color1.a);
     glClear(GL_COLOR_BUFFER_BIT);
-    glfwSwapBuffers(window);
 }
 
 /**
@@ -130,16 +144,91 @@ void windowColorRGBA(uint8_t r,uint8_t g,uint8_t b,uint8_t a)
  * @param outMin Minimum value of output
  * @param outMax Maximum value of output
  */
-double map(double x, double inMin, double inMax, double outMin, double outMax) {
+double map(double x, double inMin, double inMax, double outMin, double outMax)
+{
     return outMin + (x - inMin) * (outMax - outMin) / (inMax - inMin);
 }
 
 /**
  * Map an RGBA parameter (uint8_t), which is between 0 and 255 to be between 0 and 1
- * 
+ *
  * @param x uint8_t thats needed to be converted
- * 
+ *
  */
-float mapRGBA(uint8_t x){
-    return (float)map(x,0,255,0,1);
+float mapRGBA(uint8_t x)
+{
+    return (float)map(x, 0, 255, 0, 1);
+}
+
+char *readShaderFromFile(const char *fileName)
+{
+    FILE *file = fopen(fileName, "rb");
+    if (!file)
+    {
+        perror("Failed to open file");
+        return NULL;
+    }
+
+    // Seek to the end to find the file size
+    fseek(file, 0, SEEK_END);
+    long fileSize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    // Allocate memory for the shader text
+    char *shaderCode = (char *)malloc(fileSize + 1);
+    if (!shaderCode)
+    {
+        perror("Failed to allocate memory");
+        fclose(file);
+        return NULL;
+    }
+
+    // Read the file into the buffer
+    fread(shaderCode, 1, fileSize, file);
+    shaderCode[fileSize] = '\0'; // Null-terminate the string
+
+    fclose(file);
+    return shaderCode;
+}
+
+unsigned int CompileShader(const char *source, int type)
+{
+    unsigned int shader = glCreateShader(type);
+    glShaderSource(shader, 1, &source, NULL);
+    glCompileShader(shader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n %c", infoLog);
+    }
+    return shader;
+}
+
+unsigned int CreateShaderProgram(unsigned int ids[],int count)
+{
+    unsigned int program = glCreateProgram();
+    for (int i = 0; i < count; i++)
+    {
+        glAttachShader(program, ids[i]);
+    }
+    glLinkProgram(program);
+    for (int i = 0; i < count; i++)
+    {
+        glDeleteShader(ids[i]);
+    }
+    int success;
+    char infoLog[512];
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(program, 512, NULL, infoLog);
+        printf("ERROR::SHADER::COMPILE::FAILED\n %c", infoLog);
+        
+    }
+
+    return program;
 }
